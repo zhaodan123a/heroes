89b2e7cbcceca10ace0e2cb7bb13c12eb5bcf119
zhao <zhao1234@qq.com>
2019-07-08 17:07:37 +0800

    组件化头部和左侧

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,32 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+		<!-- 头部 -->
+    <app-header></app-header>
+		<div class="container-fluid">
+			<div class="row">
+				<!-- 左侧 -->
+				<app-spilder></app-spilder>
+				<!-- 右侧占位 -->
+				<router-view></router-view>
+			</div>
+		</div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import AppHeader from './components/AppHeader.vue'
+  import AppSpilder from './components/AppSpilder.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    AppHeader,
+    AppSpilder,
+
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,24 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+import '../node_modules/bootstrap/dist/css/bootstrap.css'
+import './assets/css/index.css'
+
+//引入路由
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+
+//导入英雄列表
+import HeroesList from './views/heroes/heroeslist.vue'
+
+
+//配置路由
+const router=new VueRouter({
+  routes:[
+    {name:'heroes',path:'/heroes',component:HeroesList}
+  ]
+})
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
