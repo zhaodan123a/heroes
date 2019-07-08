49a8b6e5368fea133d675300d6dbd4065466308f
zhao <zhao1234@qq.com>
2019-07-08 20:52:06 +0800

    注释

--- src/main.js
+++ src/main.js
@@ -11,6 +11,8 @@ import './assets/css/index.css'
 import router from './router'
 //引入axios，将axios作为全局使用
 import axios from '../node_modules/axios/dist/axios.js'
+// 因为请求中的地址部分一样，将一样的地址使用baseURL解决，然后使用地址时可以用heroes代替相同部分
+// （项目名是什么就会用什么代替）
 axios.defaults.baseURL='http://localhost:3000/'
 //将axios放到vue的原型上,以后在使用axios时就直接使用this.newaxios.不用每次引入
 Vue.prototype.newaxios=axios;
